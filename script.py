0accb366330ebb8b5cecf832caba756cb606f82a
adm li <2293151837@qq.com>
2024-11-13 00:45:13 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_and_filter():
-    url = 'https://raw.githubusercontent.com/yuanzl77/IPTV/refs/heads/main/live.txt'
+    url = 'https://raw.githubusercontent.com/yuanzl77/IPTV/refs/heads/main/live.txt,https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6.m3u'
     
     # 获取文件内容
     response = requests.get(url)
